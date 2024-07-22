338351070077ea4ba93f7bda91c8359238b38f60
Anton de Groot <adgroot@bkwi.nl>
2024-07-22 11:18:45 +0200

    051 fix turn based unit tests

--- GWT_Keezenspel-server/src/test/java/gwtks/TurnBasedTest.java
+++ GWT_Keezenspel-server/src/test/java/gwtks/TurnBasedTest.java
@@ -152,6 +152,27 @@ public class TurnBasedTest {
         assertTrue(CardsDeck.getCardsForPlayer(1).isEmpty());
         assertEquals(2,GameState.getPlayerIdTurn());
     }
+    @Test
+    void allPlayersExcept1Forfeit_RemainingPlayerKeepsPlaying(){
+        // GIVEN
+        sendForfeitMessage(0);
+        sendForfeitMessage(1);
+
+        for (int i = 0; i < 5; i++) {
+            // WHEN
+            sendValidMoveMessage(2);
+
+            // THEN
+            if(i < 4){
+                assertEquals("playerId turn NOT last round",2, GameState.getPlayerIdTurn());
+                assertEquals("cards remaining for player 2"+i,4-i, CardsDeck.getCardsForPlayer(2).size());
+            }else{
+                // last move player 2 has no more cards, then the cards should be dealt and he should have 4 cards, while player 0 is now playing
+                assertEquals("playerId turn last round", 0 , GameState.getPlayerIdTurn());
+                assertEquals("cards remaining for player 2",4, CardsDeck.getCardsForPlayer(2).size());
+            }
+        }
+    }
 
     private ArrayList<Integer> nrCardsPerPlayer(int[] nrCards){
         ArrayList<Integer> nrCarsperPlayer = new ArrayList<>();
