4bc1c7d3e505a1caa14b42b0b5bbc97e9346d784
Anton de Groot <adgroot@bkwi.nl>
2025-01-10 09:56:50 +0100

    101 change playerId from int to String

--- GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/GameStateUtil.java
+++ GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/GameStateUtil.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class GameStateUtil {
 
-    public static Pawn createPawnAndPlaceOnBoard(int playerId, TileId currentTileId){
+    public static Pawn createPawnAndPlaceOnBoard(String playerId, TileId currentTileId){
         // for creating pawns for different players
         PawnId pawnId1 = new PawnId(playerId,0);
         Pawn pawn1 = new Pawn(pawnId1, new TileId(playerId, -1));
@@ -22,23 +22,23 @@ public class GameStateUtil {
         GameState.movePawn(pawn1);
         return pawn1;
     }
-    public static Card givePlayerCard(int playerId, int nrSteps){
-        Card card = new Card(playerId, nrSteps-1);
+    public static Card givePlayerCard(String playerId, int nrSteps){
+        Card card = new Card(0, nrSteps-1);
         ADG.Games.Keezen.CardsDeck.setPlayerCard(playerId, card);
         return card;
     }
-    public static Card givePlayerAce(int playerId){
-        Card ace = new Card(playerId, 0);
+    public static Card givePlayerAce(String playerId){
+        Card ace = new Card(0, 0);
         ADG.Games.Keezen.CardsDeck.giveCardToPlayerForTesting(playerId, ace);
         return ace;
     }
-    public static Card givePlayerKing(int playerId){
-        Card king = new Card(playerId, 11);
+    public static Card givePlayerKing(String playerId){
+        Card king = new Card(0, 11);
         ADG.Games.Keezen.CardsDeck.giveCardToPlayerForTesting(playerId, king);
         return king;
     }
-    public static Card givePlayerJack(int playerId){
-        Card jack = new Card(playerId, 10);
+    public static Card givePlayerJack(String playerId){
+        Card jack = new Card(0, 10);
         ADG.Games.Keezen.CardsDeck.giveCardToPlayerForTesting(playerId, jack);
         return jack;
     }
@@ -58,14 +58,14 @@ public class GameStateUtil {
         moveMessage.setMoveType(MoveType.SWITCH);
         moveMessage.setMessageType(MessageType.MAKE_MOVE);
     }
-    public static void sendForfeitMessage(int playerId){
+    public static void sendForfeitMessage(String playerId){
         MoveMessage moveMessage = new MoveMessage();
         moveMessage.setPlayerId(playerId);
         moveMessage.setMoveType(MoveType.FORFEIT);
         moveMessage.setMessageType(MessageType.MAKE_MOVE);
         GameState.processOnForfeit(moveMessage);
     }
-    public static void sendValidMoveMessage(int playerId){
+    public static void sendValidMoveMessage(String playerId){
         // get the first pawn of the player
         Pawn pawn = new Pawn(new PawnId(playerId,0),new TileId(playerId,-1));
 
@@ -91,7 +91,7 @@ public class GameStateUtil {
         MoveResponse moveResponse = new MoveResponse();
         GameState.processOnMove(moveMessage, moveResponse);
     }
-    public static void playRemainingCards(int playerId){
+    public static void playRemainingCards(String playerId){
         int nrCards = CardsDeck.getCardsForPlayer(playerId).size();
         for (int i = 0; i < nrCards; i++) {
             sendValidMoveMessage(playerId);
@@ -104,14 +104,14 @@ public class GameStateUtil {
         }
         return result;
     }
-    public static void place4PawnsOnFinish(int playerId){
+    public static void place4PawnsOnFinish(String playerId){
         createPawnAndPlaceOnBoard(new PawnId(playerId,0) , new TileId(playerId,16));
         createPawnAndPlaceOnBoard(new PawnId(playerId,1) , new TileId(playerId,17));
         createPawnAndPlaceOnBoard(new PawnId(playerId,2) , new TileId(playerId,18));
         createPawnAndPlaceOnBoard(new PawnId(playerId,3) , new TileId(playerId,19));
     }
 
-    public static void createGame_With_NPlayers(int nrPlayers){
+    public static void createGame_With_NPlayers(String nrPlayers){
         for (int i = 0; i < nrPlayers; i++) {
             GameState.addPlayer(new Player("player"+i,"123-i"));
         }
