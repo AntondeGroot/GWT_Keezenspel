7b14a3b50c960697270f88f9bd5f61d69b6260d6
Anton de Groot <adgroot@bkwi.nl>
2025-01-10 10:03:01 +0100

    101 change playerId from int to String

--- GWT_Keezenspel-shared/src/main/java/ADG/Games/Keezen/Player.java
+++ GWT_Keezenspel-shared/src/main/java/ADG/Games/Keezen/Player.java
@@ -54,7 +54,7 @@ public class Player implements IsSerializable {
         return place > 0;
     }
 
-    public void setInActive(){
+    public void setInactive(){
         isActive = false;
     }
 
