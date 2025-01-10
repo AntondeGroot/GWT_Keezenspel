7e989d27fe564833dcbca6f64c9a62168bb3f55b
Anton de Groot <adgroot@bkwi.nl>
2025-01-10 09:09:28 +0100

    101 change playerId from int to String

--- GWT_Keezenspel-shared/src/main/java/ADG/Games/Keezen/CardsService.java
+++ GWT_Keezenspel-shared/src/main/java/ADG/Games/Keezen/CardsService.java
@@ -5,5 +5,5 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("cards")
 public interface CardsService extends RemoteService {
-    CardResponse getCards(int playerId) throws IllegalArgumentException;
+    CardResponse getCards(String playerUUID) throws IllegalArgumentException;
 }
