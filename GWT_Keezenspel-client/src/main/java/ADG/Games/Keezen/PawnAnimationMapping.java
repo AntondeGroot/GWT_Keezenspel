13c51944533d4fe653c96dbc92f70937f765ed2a
Anton de Groot <adgroot@bkwi.nl>
2025-01-21 21:10:43 +0100

    fix build

--- GWT_Keezenspel-client/src/main/java/ADG/Games/Keezen/PawnAnimationMapping.java
+++ GWT_Keezenspel-client/src/main/java/ADG/Games/Keezen/PawnAnimationMapping.java
@@ -83,7 +83,7 @@ public class PawnAnimationMapping {
     public String toString() {
         return "PawnAnimationMapping{" +
                 "pawn=" + pawn +
-                ", points=" + points.getFirst() + " - " + points.getLast() +
+                ", points=" + points.get(0) + " - " + points.get(points.size()-1) +
                 '}';
     }
 }
