2941c065656a20ca327361ad670577bfa3cc3188
Anton de Groot <adgroot@bkwi.nl>
2025-01-22 22:03:13 +0100

    116 clean up todo's

--- GWT_Keezenspel-client/src/main/java/ADG/Games/Keezen/util/PlayerUUIDUtil.java
+++ GWT_Keezenspel-client/src/main/java/ADG/Games/Keezen/util/PlayerUUIDUtil.java
@@ -4,25 +4,25 @@ import ADG.Games.Keezen.Player;
 
 import java.util.ArrayList;
 
-public class PlayerUUIDColor {
+public class PlayerUUIDUtil {
 
-    public static String colorToUUID(int colorIndex, ArrayList<Player> players){
+    public static String playerIntToUUID(int playerIndex, ArrayList<Player> players){
         for (Player p : players) {
-            if(p.getColor() == colorIndex){
+            if(p.getIndex() == playerIndex){// todo: replace get color with index
                 return p.getUUID();
             }
         }
         return "";
     }
 
-    public static int UUIDtoColor(String UUID, ArrayList<Player> players){
+    public static int UUIDtoInt(String UUID, ArrayList<Player> players){
         if(UUID == null){
             return 0;
         }
 
         for (Player p : players) {
             if(p.getUUID().equals(UUID)){
-                return p.getColor();
+                return p.getIndex();
             }
         }
         return 0;
--- GWT_Keezenspel-client/src/test/java/ADG/MoveControllerTest.java
+++ GWT_Keezenspel-client/src/test/java/ADG/MoveControllerTest.java
@@ -26,61 +26,6 @@ class MoveControllerTest {
         moveResponse = null;
 
     }
-    @Test
-    void movePawn2Tiles() {
-        // todo: the client no longer creates the pawns, the server creates them. Maybe this test should be changed, or moved/split to client/server
-        fail("Not yet implemented, maybe not needed");
-//        // GIVEN
-//        TileId endTile = new TileId("0",2);
-//        PawnId pawnId = new PawnId("0",0);
-//
-//        Board board = new Board();
-//        board.createBoard(createPlayers(2),300);
-//
-//        LinkedList<TileId> moves = new LinkedList<>();
-//        moves.add(new TileId("0",2));
-//        MoveResponse moveResponse = new MoveResponse();
-//        moveResponse.setPawnId1(pawnId);
-//        moveResponse.setMovePawn1(moves);
-//
-//        // WHEN
-//        MoveController.movePawn(moveResponse);
-//
-//        // THEN
-//        Pawn pawn = Board.getPawn(pawnId);
-//        assertEquals(pawn.getCurrentTileId().getTileNr(), endTile.getTileNr());
-//        assertEquals(Board.getPawns().size(),4*2);
-    }
-    @Test
-    void movingPawnKillsOtherPlayer() {
-        // todo: the client no longer creates the pawns, the server creates them. Maybe this test should be changed, or moved/split to client/server
-        fail("Not yet implemented, maybe not needed");
-//        Board board = new Board();
-//        board.createBoard(createPlayers(2),300);
-//        LinkedList<TileId> moves1 = new LinkedList<>();
-//        LinkedList<TileId> moves2 = new LinkedList<>();
-//
-//        Pawn pawn1 = createPawnAndPlaceOnBoard("1", new TileId("0",4));
-//        Pawn pawn2 = createPawnAndPlaceOnBoard("0", new TileId("0",5));
-//
-//        moves1.add(pawn1.getCurrentTileId());
-//        moves1.add(pawn2.getCurrentTileId());
-//        moves2.add(pawn2.getCurrentTileId());
-//        moves2.add(pawn2.getNestTileId());
-//
-//        MoveResponse moveResponse = new MoveResponse();
-//        moveResponse.setPawnId1(pawn1.getPawnId());
-//        moveResponse.setPawnId2(pawn2.getPawnId());
-//        moveResponse.setMovePawn1(moves1);
-//        moveResponse.setMovePawn2(moves2);
-//
-//        // WHEN
-//        MoveController.movePawn(moveResponse);
-//
-//        // THEN pawn 2 should be killed
-//        Pawn pawn = Board.getPawn(pawn2.getPawnId());
-//        assertEquals(pawn.getNestTileId(), pawn.getCurrentTileId());
-    }
 
     private static Pawn createPawnAndPlaceOnBoard(String playerId, TileId currentTileId){
         // for creating pawns for different players
