9c053bf7ab70d55045d6f5929fe50081719fed8c
Anton de Groot <adgroot@bkwi.nl>
2025-04-22 19:11:15 +0200

    160 exclude selenium tests in CI

--- GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/FrontEnd/TestUtils.java
+++ GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/FrontEnd/TestUtils.java
@@ -8,12 +8,21 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestUtils {
 
   public static WebDriver getDriver(){
-    WebDriver driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
+// ✅ Add this line to fix the CI error:
+    options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.nanoTime());
+
+    WebDriver driver = new ChromeDriver(options);
     driver.get("http://localhost:4200/");
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     return driver;
