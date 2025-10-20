1ad419c65cae85d2b525b3281569a27cd40cc621
Anton de Groot <adgroot@bkwi.nl>
2025-10-20 20:33:10 +0200

    202: fix tests

--- GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/ApiTests/ApiGameWithPlayersTest.java
+++ GWT_Keezenspel-server/src/test/java/ADG/Games/Keezen/ApiTests/ApiGameWithPlayersTest.java
@@ -59,4 +59,43 @@ class ApiGameWithPlayersTest {
     // cleanup
     apiHelper.stopGame(sessionId);
   }
+
+  @Test
+  void createGame_add3Players_player1isPlayingAndActiver_OtherPlayersActive() {
+    // GIVEN a game has started
+    String roomName = getRandomRoomName();
+    Player player1 = getRandomPlayer();
+    Player player2 = getRandomPlayer();
+    Player player3 = getRandomPlayer();
+    String sessionId = apiHelper.createNewGame(roomName, 3);
+
+    // WHEN you add 3 player
+    // you can't use player.uuid because that would have to be assigned belowe
+    apiHelper.addPlayerToGame(sessionId, player1);
+    apiHelper.addPlayerToGame(sessionId, player2);
+    apiHelper.addPlayerToGame(sessionId, player3);
+    apiHelper.startGame(sessionId);
+
+    // THEN they were added to the game
+    var players = apiHelper.getAllPlayersInGame(sessionId);
+
+    boolean foundPlayer = players.stream().anyMatch(p ->
+            p.get("name").equals(player1.getName()) &&
+            p.get("isActive").equals(true) &&
+            p.get("isPlaying").equals(true));
+    assertTrue(foundPlayer, "First player should be active and playing");
+    boolean foundPlayer2 = players.stream().anyMatch(p ->
+        p.get("name").equals(player2.getName()) &&
+            p.get("isActive").equals(true) &&
+            p.get("isPlaying").equals(false));
+    assertTrue(foundPlayer2, "Second player should only be active but not playing");
+    boolean foundPlayer3 = players.stream().anyMatch(p ->
+        p.get("name").equals(player3.getName()) &&
+            p.get("isActive").equals(true) &&
+            p.get("isPlaying").equals(false));
+    assertTrue(foundPlayer3, "Third player should be active but not playing");
+
+    // cleanup
+    apiHelper.stopGame(sessionId);
+  }
 }
