38bc5619cb4e9ff9244fdc75dceb7a492a94a6d6
Anton de Groot <adgroot@bkwi.nl>
2026-07-09 10:54:22 +0200

    chore: remove dead GWT_Keezenspel-shared module

--- GWT_Keezenspel-shared/src/main/java/adg/keezen/move/MessageType.java
+++
@@ -1,7 +0,0 @@
-package adg.keezen.move;
-
-
-public enum MessageType {
-  CHECK_MOVE,
-  MAKE_MOVE
-}
--- GWT_Keezenspel-shared/src/main/java/adg/keezen/move/MoveResult.java
+++
@@ -1,9 +0,0 @@
-package adg.keezen.move;
-
-
-public enum MoveResult {
-  CAN_MAKE_MOVE,
-  CANNOT_MAKE_MOVE,
-  PLAYER_DOES_NOT_HAVE_CARD,
-  INVALID_SELECTION
-}
